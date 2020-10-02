e0d1cf1d97e16c568c8177c8d685ccc02bb78954
Gui-Correa <71907354+Gui-Correa@users.noreply.github.com>
2020-10-02 20:13:56 -0300

    Add files via upload
    Programa armazena em variáveis o seu nome, sua idade e a cidade onde reside. Na sequencia exiba o conteúdo destas variáveis na tela.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercicios_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, cidade;
+            int idade;
+            Console.WriteLine("Digite seu nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua cidade");
+            cidade = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade:");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Seu nome é:" + nome);
+            Console.WriteLine("Sua cidade é:" + cidade);
+            Console.WriteLine("Sua idade é:" + idade);
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
